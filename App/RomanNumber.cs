68aa774ee3aa22d7286f146c4950c1cee8a8e791
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-09-02 13:06:39 +0100

    Add project files.

---
+++ App/RomanNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace App
+{
+    public record RomanNumber(int Value)
+    {
+        private readonly int _value = Value;
+        public int Value => _value;
+
+        public static RomanNumber Parse(string input)
+        {
+            input = input.ToUpper();
+
+            return input switch
+            {
+                "I" => new RomanNumber(1),
+                "IV" => new RomanNumber(4),
+                "V" => new RomanNumber(5),
+                "IX" => new RomanNumber(9),
+                "X" => new RomanNumber(10),
+                _ => throw new ArgumentException("Invalid Number!"),
+            };
+        }
+    }
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using App;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class RomanNumberTest
+    {
+
+
+        [TestMethod]
+        public void Parse_I_ReturnsOne()
+        {
+            RomanNumber rn = RomanNumber.Parse("I");
+            Assert.AreEqual(1, rn.Value, "I should return 1.");
+        }
+
+        [TestMethod]
+        public void Parse_IV_ReturnsFour()
+        {
+            RomanNumber rn = RomanNumber.Parse("IV");
+            Assert.AreEqual(4, rn.Value, "IV should return 4.");
+        }
+
+        [TestMethod]
+        public void Parse_V_ReturnsFive()
+        {
+            RomanNumber rn = RomanNumber.Parse("V");
+            Assert.AreEqual(5, rn.Value, "V should return 5.");
+        }
+
+        [TestMethod]
+        public void Parse_IX_ReturnsNine()
+        {
+            RomanNumber rn = RomanNumber.Parse("IX");
+            Assert.AreEqual(9, rn.Value, "IX should return 9.");
+        }
+
+        [TestMethod]
+        public void Parse_X_ReturnsTen()
+        {
+            RomanNumber rn = RomanNumber.Parse("X");
+            Assert.AreEqual(10, rn.Value, "X should return 10.");
+        }
+
+    }
+}
